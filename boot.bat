30328d233cb4a5fb02a9562ed7b1d7f4b80c16b3
Colton Gagel <coltongagel@gmail.com>
2024-06-25 21:33:17 -0700

    init 2

--- boot.bat
+++ boot.bat
@@ -1,4 +1,4 @@
 :: start streamlit app, cd to repo so files will be callable 
-cd C:\Users\colto\OneDrive\Documents
-C:\Users\colto\OneDrive\Documents\aptio_env\Scripts\activate
-python -m streamlit run C:\Users\colto\Documents\aptio_ai\home.py
+cd C:\Users\colto\Documents
+C:\Users\colto\Documents\aptio_env\Scripts\activate
+python -m streamlit run C:\Users\colto\OneDrive\Documents\aptio_ai\home.py
--- home.py
+++ home.py
@@ -3,15 +3,34 @@
 
 import streamlit as st
 import time
+from io import StringIO
+import pdfplumber
 
+# Functions 
+def extract_data(feed):
+    data = []
+    with pdfplumber.load(feed) as pdf:
+        pages = pdf.pages
+        for p in pages:
+            data.append(p.extract_tables())
+    return data
+
+
+
+# Main page 
 st.title('Aptio AI')
+st.subheader('job finder! PLACE HOLDER')
 
+radio_input = st.radio("",['Text Input','File Upload'],horizontal=True)
 
-if st.radio("",['Text Input','File Upload'],horizontal=True):
-    file = st.file_uploader("Upload your data")
+if radio_input == 'Text Input':
+    text_input = st.text_input("Paste resume contents here",label_visibility ='collapsed',placeholder ='Paste resume contents here')
+
+if radio_input == 'File Upload':
+    file = st.file_uploader("Upload resume contents",type=['doc','docx','pdf','txt'])
     if file:
-        with st.spinner("Processing your file"):
-            time.sleep(5) #<- dummy wait for demo. 
+        file_data = extract_data(file)
+        st.write(file_data)
 
-text_input = st.chat_input("What u want")
+
 
